6d9fae330c7f94f20415d11bc0f953bbf4cd9f84
ifilipis <40601736+ifilipis@users.noreply.github.com>
2025-07-16 23:09:28 +0300

    Add SaveImageOpenEXR node

--- nodes.py
+++ nodes.py
@@ -1,9 +1,10 @@
+import os
 import torch
 import numpy as np
-import torch
 import torch.nn.functional as F
 from .relighting.tonemapper import TonemapHDR
 from HDRutils.exposures import estimate_exposures
+import folder_paths
 
 def create_envmap_grid(size: int):
     """
@@ -166,12 +167,94 @@ class exposure_to_hdr:
 
         return (hrd_rgb, ldr_rgb,)
 
+
+class SaveImageOpenEXR:
+    def __init__(self):
+        try:
+            import OpenEXR
+            import Imath
+            self.OpenEXR = OpenEXR
+            self.Imath = Imath
+            self.use_openexr = True
+        except ImportError:
+            print("No OpenEXR module found, trying OpenCV...")
+            self.use_openexr = False
+            try:
+                os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+                import cv2
+                self.cv2 = cv2
+            except ImportError:
+                raise ImportError("No OpenEXR or OpenCV module found, can't save EXR")
+
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "filename_prefix": ("STRING", {"default": "ComfyUI_EXR"})
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("file_url",)
+    FUNCTION = "saveexr"
+    OUTPUT_NODE = True
+    CATEGORY = "Marigold"
+
+    def saveexr(self, images, filename_prefix):
+        import re
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(
+            filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0]
+        )
+
+        def file_counter():
+            max_counter = 0
+            for existing_file in os.listdir(full_output_folder):
+                match = re.fullmatch(rf"{filename}_(\d+)_?\.[a-zA-Z0-9]+", existing_file)
+                if match:
+                    fc = int(match.group(1))
+                    if fc > max_counter:
+                        max_counter = fc
+            return max_counter
+
+        for image in images:
+            image_np = image.cpu().numpy().astype(np.float32)
+
+            if self.use_openexr:
+                PIXEL_TYPE = self.Imath.PixelType(self.Imath.PixelType.FLOAT)
+                height, width, channels = image_np.shape
+                header = self.OpenEXR.Header(width, height)
+                half_chan = self.Imath.Channel(PIXEL_TYPE)
+                header['channels'] = dict([(c, half_chan) for c in "RGB"])
+                R = image_np[:, :, 0].tobytes()
+                G = image_np[:, :, 1].tobytes()
+                B = image_np[:, :, 2].tobytes()
+                counter = file_counter() + 1
+                file = f"{filename}_{counter:05}.exr"
+                exr_file = self.OpenEXR.OutputFile(os.path.join(full_output_folder, file), header)
+                exr_file.writePixels({'R': R, 'G': G, 'B': B})
+                exr_file.close()
+            else:
+                counter = file_counter() + 1
+                file = f"{filename}_{counter:05}.exr"
+                exr_file = os.path.join(full_output_folder, file)
+                self.cv2.imwrite(exr_file, image_np)
+
+        return (f"/view?filename={file}&subfolder=&type=output",)
+
 NODE_CLASS_MAPPINGS = {
     "chrome_ball_to_envmap": chrome_ball_to_envmap,
     "exposure_to_hdr": exposure_to_hdr,
+    "SaveImageOpenEXR": SaveImageOpenEXR,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "chrome_ball_to_envmap": "Chrome Ball to Envmap",    
+    "chrome_ball_to_envmap": "Chrome Ball to Envmap",
     "exposure_to_hdr": "Exposure to HDR",
+    "SaveImageOpenEXR": "Save Image OpenEXR",
 }
